58fe7195b70131953a3ad7c2e12c1a75ab8489cd
checkmate-bitch <sawan.dontgiveadamn@gmail.com>
2017-02-21 17:13:46 +0530

    app/components/nav.jsx: automated the process to generate lis using loop app/index.jsx: added initial states and js to automatically change title

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -1,37 +1,29 @@
 var React = require('react')
 
 let Nav = React.createClass({
+
+  getInitialState: function (props) {
+    return {
+    };
+  },
+
+  getLi: function () {
+    // console.log("nav.jsx", this.props.components);
+    const lis = this.props.components.map( comp => {
+      return <li id={comp} className="each-component">
+              <a href="#">{comp}</a>
+            </li>;
+    })
+    console.log(lis);
+    return lis;
+  },
+  
   render: function () {
     return (
       <nav>
         <h1 className="title">Bootstrap 4.0</h1>
         <ul id="components">
-          <li> <a href="#">Glyphicons</a>
-          </li>
-          <li> <a href="#">Dropdowns</a>
-          </li>
-          <li> <a href="#">Button groups</a>
-          </li>
-          <li> <a href="#">Button dropdowns</a>
-          </li>
-          <li> <a href="#">Input groups</a>
-          </li>
-          <li> <a href="#">Navs</a>
-          </li>
-          <li> <a href="#">Navbar</a>
-          </li>
-          <li> <a href="#">Alerts</a>
-          </li>
-          <li> <a href="#">Progress bars</a>
-          </li>
-          <li> <a href="#">Media object</a>
-          </li>
-          <li> <a href="#">List group</a>
-          </li>
-          <li> <a href="#">Panels</a>
-          </li>
-          <li><a href="#">Responsive embed</a></li>
-          <li><a href="#">Wells</a></li>
+          { this.getLi() }
         </ul>
       </nav>
     )
